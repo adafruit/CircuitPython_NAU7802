97cb8a80dfced998d27609e94d9a76998b97a961
Cedar Grove Maker Studios <jan@goolsbey.net>
2022-04-23 16:22:53 -0700

    pylinted; fixed .bmp "unlicense"

--- cedargrove_nau7802.py
+++ cedargrove_nau7802.py
@@ -17,8 +17,10 @@ Implementation Notes
 
 **Hardware:**
 
-* `NAU7802 FeatherWing; OSH Park project (16-SOIC version) <https://oshpark.com/shared_projects/qFvEU3Bn>`_
-* `NAU7802 FeatherWing; OSH Park project (16-DIP version) <https://oshpark.com/shared_projects/ZfryHYnc>`_
+* `NAU7802 FeatherWing; OSH Park project (16-SOIC version)
+  <https://oshpark.com/shared_projects/qFvEU3Bn>`_
+* `NAU7802 FeatherWing; OSH Park project (16-DIP version)
+  <https://oshpark.com/shared_projects/ZfryHYnc>`_
 * `SparkFun Quicc Scale (single channel) <https://www.sparkfun.com/products/15242>`_
 
 **Software and Dependencies:**
@@ -35,7 +37,7 @@ import struct
 
 from adafruit_bus_device.i2c_device import I2CDevice
 from adafruit_register.i2c_struct   import ROUnaryStruct
-from adafruit_register.i2c_struct   import UnaryStruct
+#from adafruit_register.i2c_struct   import UnaryStruct
 from adafruit_register.i2c_bits     import RWBits
 from adafruit_register.i2c_bits     import ROBits
 from adafruit_register.i2c_bit      import RWBit
@@ -58,12 +60,15 @@ _PGA      = 0x1B  # Programmable Gain Amplifier   RW
 _PWR_CTRL = 0x1C  # Power Control                 RW
 _REV_ID   = 0x1F  # Chip Revision ID              R-
 
+# pylint: disable=too-few-public-methods
 class LDOVoltage:
+    """Internal low-dropout voltage regulator settings."""
     LDO_3V0 = 0x5  # LDO 3.0 volts; _CTRL1[5:3] = 5
     LDO_2V7 = 0x6  # LDO 2.7 volts; _CTRL1[5:3] = 6
     LDO_2V4 = 0x7  # LDO 2.4 volts; _CTRL1[5:3] = 7
 
 class Gain:
+    """Analog differential amplifier gain settings."""
     GAIN_X1   = 0x0  # Gain X1; _CTRL1[2:0] = 0 (chip default)
     GAIN_X2   = 0x1  # Gain X1; _CTRL1[2:0] = 1
     GAIN_X4   = 0x2  # Gain X1; _CTRL1[2:0] = 2
@@ -74,6 +79,7 @@ class Gain:
     GAIN_X128 = 0x7  # Gain X1; _CTRL1[2:0] = 7
 
 class ConversionRate:
+    """ADC conversion rate settings."""
     RATE_10SPS  = 0x0  #  10 samples/sec; _CTRL2[6:4] = 0 (chip default)
     RATE_20SPS  = 0x1  #  20 samples/sec; _CTRL2[6:4] = 1
     RATE_40SPS  = 0x2  #  40 samples/sec; _CTRL2[6:4] = 2
@@ -81,11 +87,14 @@ class ConversionRate:
     RATE_320SPS = 0x7  # 320 samples/sec; _CTRL2[6:4] = 7
 
 class CalibrationMode:
+    """Calibration mode state settings."""
     INTERNAL = 0x0  # Offset Calibration Internal; _CTRL2[1:0] = 0 (chip default)
     OFFSET   = 0x2  # Offset Calibration System;   _CTRL2[1:0] = 2
     GAIN     = 0x3  # Gain   Calibration System;   _CTRL2[1:0] = 3
 
 class NAU7802:
+    """The primary NAU7802 class."""
+    # pylint: disable=too-many-instance-attributes
     def __init__(self, i2c_bus, address=0x2A, active_channels=1):
         """ Instantiate NAU7802; LDO 3v0 volts, gain 128, 10 samples per second
         conversion rate, disabled ADC chopper clock, low ESR caps, and PGA output
@@ -93,10 +102,8 @@ class NAU7802:
         self.i2c_device = I2CDevice(i2c_bus, address)
         if not self.reset():
             raise RuntimeError("NAU7802 device could not be reset")
-            return
         if not self.enable(True):
             raise RuntimeError("NAU7802 device could not be enabled")
-            return
         self.ldo_voltage     = '3V0'  # 3.0-volt internal analog power (AVDD)
         self._pu_ldo_source  = True   # Internal analog power (AVDD)
         self.gain            =  128   # X128
@@ -104,9 +111,13 @@ class NAU7802:
         self._adc_chop_clock =  0x3   # 0x3 = Disable ADC chopper clock
         self._pga_ldo_mode   =  0x0   # 0x0 = Use low ESR capacitors
         self._act_channels   =  active_channels
-        self._pc_cap_enable  =  0x1   # 0x1 = Enable PGA out stabilizer cap for single channel use
+        # 0x1 = Enable PGA out stabilizer cap for single channel use
+        self._pc_cap_enable  =  0x1
         if self._act_channels == 2:
-            self._pc_cap_enable  =  0x0   # 0x0 = Disable PGA out stabilizer cap for dual channel use
+            # 0x0 = Disable PGA out stabilizer cap for dual channel use
+            self._pc_cap_enable  =  0x0
+        self._calib_mode     = None  # Initialize for later use
+        self._adc_out        = None  # Initialize for later use
 
     # DEFINE I2C DEVICE BITS, NYBBLES, BYTES, AND REGISTERS
     _rev_id         = ROBits(4, _REV_ID, 0, 1, False)  # Chip Revision                         R-
@@ -154,8 +165,6 @@ class NAU7802:
             time.sleep(0.400)  # 400ms settling time for 10SPS
         else:
             raise ValueError("Invalid Channel Number")
-            return
-        return
 
     @property
     def ldo_voltage(self):
@@ -164,9 +173,8 @@ class NAU7802:
     @ldo_voltage.setter
     def ldo_voltage(self, voltage='EXTERNAL'):
         """Select the LDO Voltage. Valid voltages are '2V4', '2V7', '3V0'."""
-        if not ('LDO_' + voltage in dir(LDOVoltage)):
+        if not 'LDO_' + voltage in dir(LDOVoltage):
             raise ValueError("Invalid LDO Voltage")
-            return
         self._ldo_voltage = voltage
         if self._ldo_voltage == '2V4':
             self._c1_vldo_volts = LDOVoltage.LDO_2V4
@@ -174,7 +182,6 @@ class NAU7802:
             self._c1_vldo_volts = LDOVoltage.LDO_2V7
         elif self._ldo_voltage == '3V0':
             self._c1_vldo_volts = LDOVoltage.LDO_3V0
-        return
 
     @property
     def gain(self):
@@ -184,9 +191,8 @@ class NAU7802:
     def gain(self, factor=1):
         """Select PGA gain factor. Valid values are '1, 2, 4, 8, 16, 32, 64,
         and 128."""
-        if not ('GAIN_X' + str(factor) in dir(Gain)):
+        if not 'GAIN_X' + str(factor) in dir(Gain):
             raise ValueError("Invalid Gain Factor")
-            return
         self._gain = factor
         if self._gain == 1:
             self._c1_gains = Gain.GAIN_X1
@@ -204,7 +210,6 @@ class NAU7802:
             self._c1_gains = Gain.GAIN_X64
         elif self._gain == 128:
             self._c1_gains = Gain.GAIN_X128
-        return
 
     def enable(self, power=True):
         """Enable(start) or disable(stop) the internal analog and digital
@@ -253,9 +258,8 @@ class NAU7802:
     def calibrate(self, mode='INTERNAL'):
         """ Perform the calibration procedure. Valid calibration modes
         are 'INTERNAL', 'OFFSET', and 'GAIN'. True if successful."""
-        if not (mode in dir(CalibrationMode)):
+        if not mode in dir(CalibrationMode):
             raise ValueError("Invalid Calibration Mode")
-            return
         self._calib_mode = mode
         if self._calib_mode == 'INTERNAL':  # Internal PGA offset (zero setting)
             self._c2_cal_mode = CalibrationMode.INTERNAL
