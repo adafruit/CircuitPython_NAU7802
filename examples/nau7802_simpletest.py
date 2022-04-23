a0ec0b02eccf838eefec0c06f3707fd5eaa22c3a
Cedar Grove Maker Studios <jan@goolsbey.net>
2022-04-23 15:43:44 -0700

    initial commit

--- examples/nau7802_simpletest.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
-# SPDX-FileCopyrightText: Copyright (c) 2022 JG for Cedar Grove Maker Studios
-#
-# SPDX-License-Identifier: Unlicense
